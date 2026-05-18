1e300f6713267a5a0f41aadd89d4a60472142d68
Daniel <nwaujudaniel@gmail.com>
2026-05-18 13:31:02 +0100

    upgraded supabase.js file

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_SUPABASE_ANON_KEY",
+  "https://vqkwqjaynzaezumvqtmu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxa3dxamF5bnphZXp1bXZxdG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4ODQ5MjMsImV4cCI6MjA5NDQ2MDkyM30.J1OYlqoZiFWUaFJIWE0wzRI9kOuxa8qA3GA9TRrnZio",
   {
     auth: {
       persistSession: true,
